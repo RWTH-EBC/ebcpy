ec1b0c3a506cac173f47384ee2eb522bdf53cede
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2021-02-03 08:20:12 +0100

    Remove AixCalTest and use totalModels, add working example

--- ebcpy/examples/fmu_api_example.py
+++ ebcpy/examples/fmu_api_example.py
@@ -5,23 +5,30 @@ If not, please raise an issue.
 """
 
 import os
-from ebcpy.simulationapi import py_fmi
+from ebcpy.simulationapi import fmu
 
 
 def setup_fmu_api(cd=None):
     """
-    Todo
+    Function to show how to setup the FMU_API.
+
+    :param str,os.path.normpath cd:
+        Default is the current python working directory.
+        A testzone-folder is created to keep everything ordered.
+        Pass another directory to work where you want.
+    :return: The FMU_API created in the function
+    :rtype: fmu.FMU_API
     """
     # Define path in which you want ot work:
     if cd is None:
         cd = os.path.normpath(os.path.join(os.getcwd(), "testzone"))
 
     # Define the name of your model and setup the simulation api of choice
-    model_name = os.path.normpath(os.path.join(os.path.dirname(__file__),
-                                              "Modelica",
-                                              "AixCalTest_TestModel.fmu"))
+    model_name = os.path.join(os.path.dirname(__file__),
+                              "Modelica",
+                              "TestModel.fmu")
     # Setup the dymola api
-    fmu_api = py_fmi.FMU_API(cd, model_name)
+    fmu_api = fmu.FMU_API(cd, model_name)
     return fmu_api
 
 
@@ -29,8 +36,10 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     # Setup the dymola-api:
     FMU_API = setup_fmu_api()
-    FMU_API.set_sim_setup({"stopTime": 86400,
+    FMU_API.set_sim_setup({"stopTime": 3600,
                            "resultNames": ["heater1.heatPorts[1].T"]})
     res = FMU_API.simulate()
     plt.plot(res["heater1.heatPorts[1].T"])
+    # Close the api to remove the created files:
+    FMU_API.close()
     plt.show()
